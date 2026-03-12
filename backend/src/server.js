4dcc3f42be9f0b048c61c04cb9d461289c8a0fc9
Piyush Jajoo <piyushjajo04@gmail.com>
2026-03-12 18:13:52 +0530

    fix: going 3 levels up to reach frontend dist on Render

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,10 +17,10 @@ app.use("/api/messages", messageRoutes);
 
 //make ready for deployment
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../../frontend/dist")));
 
   app.use((req, res) => {
-    res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../../../frontend/dist/index.html"));
   });
 }
 
